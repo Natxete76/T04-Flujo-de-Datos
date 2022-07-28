94a10b5dc48c3d5c223d869d3f115388a754c777
Ignacio Garcia Vazquez <natxoga@gmail.com>
2022-07-28 09:40:06 +0200

    T04

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace T04_Flujo_de_Datos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            int b = 8;
+            int c = 12;
+            int d = a + b + c;
+            Console.WriteLine(d);
+            Console.ReadKey();
+        }
+    }
+}
